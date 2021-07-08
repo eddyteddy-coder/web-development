0b4c8f4e9c675ecc03c59c782e057e78ea4c27ec
eddyteddy-coder <85308460+eddyteddy-coder@users.noreply.github.com>
2021-07-09 01:43:50 +0530

    Add files via upload

---
+++ day14/day14.js
@@ -0,0 +1,21 @@
+function func1(){
+  document.querySelector("button").innerHTML="aniyo";
+
+}
+function func2(){
+  alert("You pressed a key inside the input field");
+}
+function func3(){
+  var x=document.getElementById("no");
+  x.value="";
+
+}
+function func5(){
+  document.getElementById("button2").style.color="hotpink";
+  var a=document.getElementById("button2");
+  a.innerHTML="kumawo";
+}
+function func4(){
+  document.getElementById("button2").style.color="black";
+  document.getElementById("button2").innerHTML="visit gain!";
+}
