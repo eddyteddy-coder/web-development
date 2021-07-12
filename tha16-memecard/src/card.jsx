75b901abde9dd2da46272e8b4c69216e489cedbb
eddyteddy-coder <85308460+eddyteddy-coder@users.noreply.github.com>
2021-07-12 22:55:25 +0530

    Add files via upload

---
+++ tha16-memecard/src/card.jsx
@@ -0,0 +1,13 @@
+import "./card.css"
+const Card=()=>{
+    return(
+        <div className="Card">
+            <div className="img">
+             
+              <img src="https://www.testbytes.net/wp-content/uploads/2019/06/Untitled-63.png"></img>
+            </div>
+            <h2>programming memes</h2>
+        </div>
+    )
+}
+export default Card;
