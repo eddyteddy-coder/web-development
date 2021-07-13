b76a1c203fdc62d432f36c6d048952874d5233d7
eddyteddy-coder <85308460+eddyteddy-coder@users.noreply.github.com>
2021-07-13 19:08:55 +0530

    Create day19.jsx

---
+++ day19.jsx
@@ -0,0 +1,14 @@
+import React from  "react";
+import Day19Card from "./Day19Card";
+
+
+function Day19()
+{
+    return(
+        <>
+        <Day19Card/>
+        </>
+    );
+
+}
+export default day19;
