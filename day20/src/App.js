846b264bb70ccfab38c7967aba8a6ff88749eead
eddyteddy-coder <85308460+eddyteddy-coder@users.noreply.github.com>
2021-08-16 16:18:47 +0530

    Add files via upload

---
+++ day20/src/App.js
@@ -0,0 +1,14 @@
+import Card from "./card"
+import "./App.css"
+import Day20 from "./day20"
+function App(){
+    return(
+        <div className="App">
+            <Day20 />
+            
+            
+        </div>
+
+    );
+}
+export default App;
